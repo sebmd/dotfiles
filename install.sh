2a86338013e7d1e8d036734d7975ac3f9155047d
Jan WAN LAN <jan.wan.lan@gmail.com>
2020-06-28 17:16:37 +0200

    2020-06-28-17:16:37

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s $PWD/.bashrc ~/.bashrc
+ln -s $PWD/.aliases ~/.aliases
 ln -s $PWD/bin ~/bin
 
 cd ~/bin && funkcje -i
+
+vim -c ":PlugInstall" -c ":qa"
