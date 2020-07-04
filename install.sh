f49bf9e9cdd2259e40a7f5afdba55f6f042186b8
Jan WAN LAN <jan.wan.lan@gmail.com>
2020-07-04 12:54:52 +0200

    2020-07-04-12:54:52

--- install.sh
+++ install.sh
@@ -10,26 +10,20 @@ while IFS='' read -r LINE || [[ -n "$LINE" ]]; do
     if [ ! -L $LINE ]; then
         cp -R $LINE $BACKUP_DIR/
         rm -rf $LINE
-        ln -s $SCRIPT_DIR/$line $line
+        ln -s $SCRIPT_DIR/$LINE $LINE
     fi
 done < "$SCRIPT_DIR/files"
 
-ln -s $PWD/.vim ~/.vim
 mkdir -p ~/.vim/undo
 mkdir -p ~/.vim/view
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-ln -s $PWD/.bashrc ~/.bashrc
-ln -s $PWD/.aliases ~/.aliases
-ln -s $PWD/bin ~/bin
 
 vim -c ":PlugInstall" -c ":qa"
 
 mkdir -p $HOME/notes
 mkdir -p $HOME/tmp
 mkdir -p $HOME/.config/zathura
-ln -s $PWD/.config/zathura $HOME/.config/zathura
 
 # stworzenie linków symbolicznych w katalogu ~/bin
 cd ~/bin && funkcje -i
