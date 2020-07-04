37689b8fa25edf8639b85e34a0c2ed528ae1eb7d
Jan WAN LAN <jan.wan.lan@gmail.com>
2020-07-04 13:06:22 +0200

    2020-07-04-13:06:22

--- install.sh
+++ install.sh
@@ -2,7 +2,14 @@
 
 BACKUP_DIR=$HOME/.backup
 SCRIPT_DIR=$PWD
-mkdir $BACKUP_DIR
+
+mkdir -p $BACKUP_DIR
+mkdir -p $HOME/.vim
+mkdir -p $HOME/.vim/undo
+mkdir -p $HOME/.vim/view
+mkdir -p $HOME/notes
+mkdir -p $HOME/tmp
+mkdir -p $HOME/.config/zathura
 
 cd $HOME
 
@@ -14,16 +21,11 @@ while IFS='' read -r LINE || [[ -n "$LINE" ]]; do
     fi
 done < "$SCRIPT_DIR/files"
 
-mkdir -p ~/.vim/undo
-mkdir -p ~/.vim/view
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+# przygotowanie edytora Vim
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim -c ":PlugInstall" -c ":qa"
 
-mkdir -p $HOME/notes
-mkdir -p $HOME/tmp
-mkdir -p $HOME/.config/zathura
-
-# stworzenie linków symbolicznych w katalogu ~/bin
-cd ~/bin && funkcje -i
+# stworzenie linków symbolicznych w katalogu $HOME/bin
+cd $HOME/bin && funkcje -i
